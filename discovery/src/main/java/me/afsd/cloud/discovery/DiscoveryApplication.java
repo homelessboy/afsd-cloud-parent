8d8caf719649f3ea815a6479e4d07ff64f4f8568
homelessboy <416422546@qq.com>
2016-05-06 13:39:33 +0800

    add: discovery module

---
+++ discovery/src/main/java/me/afsd/cloud/discovery/DiscoveryApplication.java
@@ -0,0 +1,17 @@
+package me.afsd.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: afsd
+ * @version: ${VERSION}
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
